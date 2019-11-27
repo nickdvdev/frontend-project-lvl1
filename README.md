f89db0cabadd274e5d01c6cb1845e57a92cc9247
jakethefriendlydog <donotwannabecontrolled@gmail.com>
2019-11-27 21:55:29 +0800

    logic for brain-even, asciinema

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-CodeClimate Maintainability Badge
+<p>CodeClimate Maintainability Badge</p>
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 
-CodeClimate Test Coverage Badge
+<p>CodeClimate Test Coverage Badge</p>
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/test_coverage"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage" /></a>
 
-Travic CI
+<p>Travic CI</p>
 <img src="https://travis-ci.org/JakeTheFriendlyDog/frontend-project-lvl1.svg?branch=master" />
+
+<p>Demonstration</p>
+<a href="https://asciinema.org/a/Q1Ar8x92rZvj2D3skCXygtjN7" target="_blank"><img src="https://asciinema.org/a/Q1Ar8x92rZvj2D3skCXygtjN7.svg" /></a>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const gameRounds = 3;
+const maxNum = 1000;
+
+export const greet = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name, please? ');
+  console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
+  const name = readlineSync.question('May I have your name, please?\n');
   console.log(`Hello, ${name}!`);
+  return name;
+};
+
+const generateRandomNum = (max) => Math.floor(Math.random() * max);
+
+export const gameIsEven = (times) => {
+  const name = greet();
+  for (let i = 0; i < times; i += 1) {
+    const question = generateRandomNum(maxNum);
+    const correctAnswer = (question % 2 === 0) === true ? 'yes' : 'no';
+    const answer = (readlineSync.question(`Question: ${question}\n`));
+    console.log(`Your answer: ${answer}`);
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    }
+    if (answer !== correctAnswer) {
+      return console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`);
+    }
+  }
+  return console.log(`Congratulations, ${name}!`);
 };
