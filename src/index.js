af597e639d6f2127272f95036447f971104b0d66
jakethefriendlydog <donotwannabecontrolled@gmail.com>
2020-02-28 23:47:23 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import readlineSync from 'readline-sync';
 import { car, cdr } from '@hexlet/pairs';
 import { roundsCount } from './utils';
 
-const playGame = (chooseGameType, condition) => {
+const playGame = (generateGameType, condition) => {
   console.log('Welcome to the Brain Games!');
   console.log(condition);
   const name = readlineSync.question('May I have your name, please?\n');
   console.log(`Hello, ${name}!`);
   for (let i = 0; i < roundsCount; i += 1) {
-    const game = chooseGameType();
+    const game = generateGameType();
     const correctAnswer = cdr(game);
     const question = car(game);
     const answer = readlineSync.question(`Question: ${question}\n`);
