b79aec3d2c1e81fa95baff52c6fb93d084148f81
jakethefriendlydog <donotwannabecontrolled@gmail.com>
2020-03-02 22:19:31 +0300

    refactor

--- src/games/progression-maker.js
+++ src/games/progression-maker.js
@@ -4,28 +4,25 @@ import { generateNum, minNum, maxNum } from '../utils';
 
 const progressionLength = 10;
 
-const makeProgression = () => {
+const makeProgression = (start, diff) => {
   const progression = [];
-  const difference = generateNum(minNum, maxNum);
-  const startPoint = generateNum(minNum, maxNum);
   for (let i = 0; i < progressionLength; i += 1) {
-    progression.push(startPoint + (difference * i));
+    progression.push(start + (diff * i));
   }
   return progression;
 };
 
 const prepareArithProgGame = () => {
-  const progression = makeProgression(progressionLength);
+  const startPoint = generateNum(minNum, maxNum);
+  const difference = generateNum(minNum, maxNum);
+  const progression = makeProgression(startPoint, difference);
   const indexOfHiddenNum = generateNum(0, progressionLength - 1);
   let question = '';
   for (let i = 0; i < progression.length; i += 1) {
-    if (i !== indexOfHiddenNum) {
-      question = `${question}${progression[i]} `;
-    }
-    if (i === indexOfHiddenNum) {
-      question = `${question}.. `;
-    }
+    question = i === indexOfHiddenNum ? `${question}.. `
+      : `${question}${progression[i]} `;
   }
+  question = question.trim();
   const correctAnswer = progression[indexOfHiddenNum].toString();
   return cons(question, correctAnswer);
 };
