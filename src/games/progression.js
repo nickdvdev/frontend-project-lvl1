e2bd749e0b0971c146172290076409aef36fde0c
jakethefriendlydog <donotwannabecontrolled@gmail.com>
2020-03-16 10:26:19 +0300

    refactor

--- src/games/progression.js
+++ src/games/progression.js
@@ -12,7 +12,7 @@ const makeProgression = (start, diff) => {
   return progression;
 };
 
-const prepareArithProgGame = () => {
+const prepareProgressionGame = () => {
   const startPoint = generateNum(minNum, maxNum);
   const difference = generateNum(minNum, maxNum);
   const progression = makeProgression(startPoint, difference);
@@ -23,13 +23,12 @@ const prepareArithProgGame = () => {
       ? `${question}.. `
       : `${question}${progression[i]} `;
   }
-  question = question.trim();
   const correctAnswer = progression[hiddenNumberIndex].toString();
-  return cons(question, correctAnswer);
+  return cons(question.trim(), correctAnswer);
 };
 
 const condition = 'What number is missing in the progression?';
 
 export default () => {
-  playGame(prepareArithProgGame, condition);
+  playGame(prepareProgressionGame, condition);
 };
